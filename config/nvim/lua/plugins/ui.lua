9cd19ff73f85b179f70c8f11f9f4df2902ab8bfc
itzzsauravp <social.saurav2003@gmail.com>
2025-05-13 08:42:42 +0545

    Fixed neo-tree black bar bug

---
+++ config/nvim/lua/plugins/ui.lua
@@ -0,0 +1,22 @@
+require("mini.animate").setup({
+  scroll = {
+    enable = true,
+    timing = require("mini.animate").gen_timing.linear({ duration = 0 }),
+  },
+  resize = {
+    enable = true,
+    timing = require("mini.animate").gen_timing.linear({ duration = 0 }),
+  },
+  cursor = {
+    enable = true,
+    timing = require("mini.animate").gen_timing.linear({ duration = 0 }),
+  },
+  open = {
+    enable = true,
+    timing = require("mini.animate").gen_timing.linear({ duration = 0 }),
+  },
+  close = {
+    enable = true,
+    timing = require("mini.animate").gen_timing.linear({ duration = 0 }),
+  },
+})
