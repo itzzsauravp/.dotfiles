4aaec013aae5bb07fc972c3a8de0aad90cc40e50
itzzsauravp <social.saurav2003@gmail.com>
2025-05-15 23:33:14 +0545

    Adds configs for glazeWM

--- sync.sh
+++ sync.sh
@@ -34,5 +34,4 @@ link_file "$DOTFILES_DIR/config/wezterm.lua" "/mnt/c/Users/your_user/AppData/Loc
 
 echo -e "\n📤 Pushing to Git with commit message: \"$COMMIT_MSG\""
 cd "$DOTFILES_DIR" && git add . && git commit -m "$COMMIT_MSG" && git push
-
 echo -e "\n✅ Dotfiles sync complete!\n"
