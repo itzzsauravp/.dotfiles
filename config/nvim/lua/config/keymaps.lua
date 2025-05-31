468f46c122a45c701d213b827eea409a5c1c199f
itzzsauravp <social.saurav2003@gmail.com>
2025-05-31 20:35:02 +0545

    updates yank and paste to work with system clipboard

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -7,3 +7,13 @@ vim.keymap.set("i", "jk", "<Esc>", { desc = "Exit insert mode with jk" })
 vim.keymap.set("n", "<leader>tw", function()
   vim.opt.wrap = not vim.opt.wrap:get()
 end, { desc = "Toggle Word Wrap" })
+
+-- Requires 'xclip' installed for this check to see using :checkhealth, check under "clipboard"
+
+-- yank to system clipboard by default
+vim.keymap.set({ "n", "v" }, "y", '"+y', { desc = "Yank to clipboard" })
+vim.keymap.set("n", "Y", '"+Y', { desc = "Yank line to clipboard" })
+
+-- paste from system clipboard by default
+vim.keymap.set({ "n", "v" }, "p", '"+p', { desc = "Paste from clipboard" })
+vim.keymap.set({ "n", "v" }, "P", '"+P', { desc = "Paste from clipboard" })
