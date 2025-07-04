4a50963fd68e68ba127917f60986048f35c2dd62
itzzsauravp <social.saurav2003@gmail.com>
2025-07-04 16:13:40 +0000

    updates rosepine background & add vscode vim binds

--- config/nvim/lua/plugins/rose-pine.lua
+++ config/nvim/lua/plugins/rose-pine.lua
@@ -6,23 +6,24 @@ return {
       disable_background = true,
       disable_float_background = true,
     })
+
     vim.cmd("colorscheme rose-pine")
 
-    -- Set black background for everything
+    -- Make everything transparent
     vim.cmd([[
-      highlight Normal guibg=#000000
-      highlight NormalNC guibg=#000000
-      highlight Pmenu guibg=#000000
-      highlight FloatBorder guibg=#000000
-      highlight NormalFloat guibg=#000000
-      highlight TelescopeNormal guibg=#000000
-      highlight TelescopeBorder guibg=#000000
-      highlight HarpoonBorder guibg=#000000
-      highlight VertSplit guibg=#000000
-      highlight StatusLine guibg=#000000
-      highlight WinSeparator guibg=#000000
-      highlight WhichKeyFloat guibg=#000000
-      highlight CursorLine guibg=#000000
+      highlight Normal guibg=NONE
+      highlight NormalNC guibg=NONE
+      highlight Pmenu guibg=NONE
+      highlight FloatBorder guibg=NONE
+      highlight NormalFloat guibg=NONE
+      highlight TelescopeNormal guibg=NONE
+      highlight TelescopeBorder guibg=NONE
+      highlight HarpoonBorder guibg=NONE
+      highlight VertSplit guibg=NONE
+      highlight StatusLine guibg=NONE
+      highlight WinSeparator guibg=NONE
+      highlight WhichKeyFloat guibg=NONE
+      highlight CursorLine guibg=NONE
     ]])
   end,
 }
